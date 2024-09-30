e787e0cdd896011c71d1620dcf2bbaf0ce5cc73c
worldbeater2 <dennisbamfo2011@gmail.com>
2024-09-30 01:49:21 +0000

    Initial commit

---
+++ bamfo-app/src/App.jsx
@@ -0,0 +1,21 @@
+
+import './App.css'
+import Homepage from './pages/Homepage'
+
+
+
+function App() {
+
+
+  return (
+    <>
+
+    <Homepage />
+
+  
+
+    </>
+  )
+}
+
+export default App
---
+++ bamfo-app/src/pages/Homepage.tsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import Footer from '../units/Footer'
+import Header from '../units/Header'
+Header
+
+
+const Homepage = () => {
+  return (
+    
+    <>
+    <Header />
+    <div>
+      <h1>Homepage</h1>
+    </div>
+    <Footer />
+    </>
+  )
+}
+
+export default Homepage
